be1f771b999d50c516ff1c0835ca7bf5db0724ab
jaimevz27 <jaime@tecnovaconsulting.com>
2023-06-12 19:22:14 -0500

    added Helpers

--- Source/Helpers/Extensions/AVAsset+Extensions.swift
+++ Source/Helpers/Extensions/AVAsset+Extensions.swift
@@ -63,4 +63,20 @@ extension AVAsset {
 
         return exportSession
     }
+    
+    var fullRange: CMTimeRange {
+        return CMTimeRange(start: .zero, duration: duration)
+    }
+    
+    func trimmedComposition(_ range: CMTimeRange) -> AVAsset {
+        guard CMTimeRangeEqual(fullRange, range) == false else {return self}
+
+        let composition = AVMutableComposition()
+        try? composition.insertTimeRange(range, of: self, at: .zero)
+
+        if let videoTrack = tracks(withMediaType: .video).first {
+            composition.tracks.forEach {$0.preferredTransform = videoTrack.preferredTransform}
+        }
+        return composition
+    }
 }
---
+++ Source/Helpers/Extensions/CMTime+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Degusta Dev on 06/12/23.
+//
+
+import CoreMedia
+
+extension CMTime {
+    var displayString: String {
+        let offset = TimeInterval(seconds)
+        let numberOfNanosecondsFloat = (offset - TimeInterval(Int(offset))) * 1000.0
+        let nanoseconds = Int(numberOfNanosecondsFloat)
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = [.minute, .second]
+        return String(format: "%@.%03d", formatter.string(from: offset) ?? "00:00", nanoseconds)
+    }
+}
